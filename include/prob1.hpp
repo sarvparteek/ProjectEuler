c3c4daabfa116219456497b4634021d8e11a83f0
sarvparteek <sarvps@umich.edu>
2020-07-12 19:28:05 -0400

    Add solution to Problem 2

--- include/prob1.hpp
+++ include/prob1.hpp
@@ -1,7 +1,7 @@
 /*!
  * @brief Solution for Problem 1 : Multiples of 3 and 5
  * @author Sarv Parteek Singh
- * @date July-05-2020
+ * @date July-12-2020
  * @details If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of
  *          these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
  */
@@ -13,6 +13,13 @@
 #include <cassert> // for assert
 #include <iostream> // for cout, during testing
 
+/*!
+ * @brief Finds the sum of whole numbers (up to a limit) divisible by one of the two input numbers
+ * @param[in] num1 First number
+ * @param[in] num2 Second number
+ * @param[in] limit Limit up to which multiples are to be found
+ * @return Sum of the multiple of the two numbers
+ */
 uint64_t multipleSum(uint32_t const &num1, uint32_t const &num2, uint32_t const &limit)
 {
     uint64_t sum = 0;
--- src/shared_main.cpp
+++ src/shared_main.cpp
@@ -5,9 +5,12 @@
  */
 
 #include "prob1.hpp"
+#include "prob2.hpp"
 
 int main()
 {
+    // TODO - 2020-07-12 : Switch to using a map between number and function poiters and iterate through the map */
     testProblem1();
+    testProblem2();
     return 0;
 }
